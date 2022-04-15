9922a32c79ac8ca43cc850c2b2b51b48e22cdf74
Julian Krenz <julian@kre.nz>
2022-04-15 23:28:01 +0200

    Icon fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,12 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-manifest",
+    //   options: {
+    //     icon: "src/images/icon.png",
+    //   },
+    // },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
