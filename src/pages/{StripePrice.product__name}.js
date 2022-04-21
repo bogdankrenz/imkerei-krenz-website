57f9d8f2c68dd89e99d6bcbf6eeb24bffbc2a663
Bogdan Krenz <bogdan.krenz@wirbauen.digital>
2022-04-21 10:01:43 +0200

    fixed header, current state WIP

--- src/pages/{StripePrice.product__name}.js
+++ src/pages/{StripePrice.product__name}.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import Layout from "../components/layout";
+import Helmet from "react-helmet";
+import Cart from "../components/cart";
 
+import Layout from "../components/layout";
 import Flower from "../images/flower3.svg";
 import Pic from "../images/honey.png";
 
@@ -29,54 +31,71 @@ const DetailPage = (props) => {
   // TODO: Create single query for detail pase
   console.log(props);
   return (
-    <Layout>
-      <div style={{ backgroundColor: "#E2F0FF", marginTop: "8px" }}>
-        <div className="columns" style={{ paddingTop: "115px" }}>
-          <div
-            className="column is-half is-flex is-horizontal-center"
-            style={{
-              backgroundImage: `url(${Flower})`,
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              backgroundSize: "100%",
-            }}
-          >
-            <img src={Pic} style={{ width: "60%" }} alt="honey" />
-          </div>
-          <div className="column">
-            <p
+    <>
+      <Helmet>
+        <html lang="de" />
+      </Helmet>
+      <Layout>
+        {/* TODO: move styles into dedicated folder/file  */}
+        <div style={{ backgroundColor: "#E2F0FF", marginTop: "8px" }}>
+          <div className="columns" style={{ paddingTop: "115px" }}>
+            <div
+              className="column is-half is-flex is-horizontal-center"
               style={{
-                fontFamily: "Lota Grotesque Alt 2",
-                fontStyle: "normal",
-                fontWeight: 700,
-                fontSize: "40px",
-                lineHeight: "44px",
-                marginTop: "43px",
+                backgroundImage: `url(${Flower})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "100%",
               }}
             >
-              Lavendelblüten Honig
-            </p>
-            <p>7,00 €</p>
-            <p>500 g</p>
-            <p>Grundpreis 26,56 € / kg</p>
-            <p>inkl. MwSt.</p>
-            <p>Versand wird beim Checkout berechnet</p>
-            <p>Lieferzeit: 2 bis 4 Werktage</p>
-            <p>Anzahl:</p>
-            <span>
-              <button onClick={decrementCount}>-</button>
-              <input
-                type="number"
-                min={1}
-                value={count}
-                onChange={handleChange}
-              />
-              <button onClick={incrementCount}>+</button>
-            </span>
+              <img src={Pic} style={{ width: "60%" }} alt="honey" />
+            </div>
+            <div className="column">
+              <p
+                style={{
+                  fontFamily: "Lota Grotesque Alt 2",
+                  fontStyle: "normal",
+                  fontWeight: 700,
+                  fontSize: "40px",
+                  lineHeight: "44px",
+                  marginTop: "43px",
+                }}
+              >
+                Lavendelblüten Honig
+              </p>
+              <p
+                style={{
+                  fontFamily: "Lota Grotesque Alt 2",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  fontSize: "32px",
+                  lineHeight: "36px",
+                  marginTop: "16px",
+                }}
+              >
+                7,00 €
+              </p>
+              <p>500 g</p>
+              <p>Grundpreis 26,56 € / kg</p>
+              <p>inkl. MwSt.</p>
+              <p>Versand wird beim Checkout berechnet</p>
+              <p>Lieferzeit: 2 bis 4 Werktage</p>
+              <p>Anzahl:</p>
+              <span>
+                <button onClick={decrementCount}>-</button>
+                <input
+                  type="number"
+                  min={1}
+                  value={count}
+                  onChange={handleChange}
+                />
+                <button onClick={incrementCount}>+</button>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
